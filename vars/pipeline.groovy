466c53f470bcd7d3e0f581b8fbd89f8f350e45df
sharanya123-ty <sharavin422@gmail.com>
2025-01-04 21:27:06 +0530

    Update pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,18 +1,28 @@
-def runFullPipeline(String artifactPath) {
+def check_out() {
+    echo 'Checking out..'
+    checkout scm
+
+def setupJava() {
     echo 'Setting up Java 17...'
     sh 'sudo apt update'
     sh 'sudo apt install -y openjdk-17-jdk'
+}
 
+def setupMaven() {
     echo 'Setting up Maven...'
     sh 'sudo apt install -y maven'
+}
 
+def runApplication() {
     echo 'Running Spring Boot application...'
     sh 'nohup mvn spring-boot:run &'
     sleep(time: 15, unit: 'SECONDS')
 
     def publicIp = sh(script: "curl -s https://checkip.amazonaws.com", returnStdout: true).trim()
     echo "The application is running and accessible at: http://${publicIp}:8080"
+}
 
+def validateApp() {
     echo 'Validating that the app is running...'
     def response = sh(script: 'curl --write-out "%{http_code}" --silent --output /dev/null http://localhost:8080', returnStdout: true).trim()
     if (response == "200") {
@@ -21,10 +31,14 @@ def runFullPipeline(String artifactPath) {
         echo "The app failed to start. HTTP response code: ${response}"
         error("The app did not start correctly!")
     }
+}
 
+def stopApplication() {
     echo 'Gracefully stopping the Spring Boot application...'
     sh 'mvn spring-boot:stop'
+}
 
+def artifactPath(String path) {
     echo 'Uploading artifact...'
-    archiveArtifacts artifacts: artifactPath, allowEmptyArchive: true
+    archiveArtifacts artifacts: path, allowEmptyArchive: true
 }
